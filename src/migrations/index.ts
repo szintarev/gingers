be358c8929529e098c28fb9992ca88887b1530b1
Ante Sosigić <112810827+AnteSosig@users.noreply.github.com>
2026-03-19 22:06:47 +0100

    zlaja

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,1 @@
-import * as migration_20260318_125734 from './20260318_125734';
-
-export const migrations = [
-  {
-    up: migration_20260318_125734.up,
-    down: migration_20260318_125734.down,
-    name: '20260318_125734'
-  },
-];
+export const migrations = [];
