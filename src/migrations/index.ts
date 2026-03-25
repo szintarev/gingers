a5ce04b9cf6b5cca2f19c5d3c3df2154a92f5c6f
Ante Sosigić <112810827+AnteSosig@users.noreply.github.com>
2026-03-25 18:48:40 +0100

    skajrim fix?

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,8 +2,6 @@ import * as migration_20260324_160627 from './20260324_160627';
 
 export const migrations = [
   {
-    up: migration_20260324_160627.up,
-    down: migration_20260324_160627.down,
-    name: '20260324_160627'
+
   },
 ];
