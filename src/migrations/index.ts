9e4114ee474201143336a0ec1b7e782fd052bdb1
Bojan <96013970+Turtlebole@users.noreply.github.com>
2026-04-05 04:12:50 +0200

    add initial database migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20260405_014641 from './20260405_014641';
+import * as migration_20260405_020145 from './20260405_020145';
 
 export const migrations = [
   {
-    up: migration_20260405_014641.up,
-    down: migration_20260405_014641.down,
-    name: '20260405_014641'
+    up: migration_20260405_020145.up,
+    down: migration_20260405_020145.down,
+    name: '20260405_020145'
   },
 ];
